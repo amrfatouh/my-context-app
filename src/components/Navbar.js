4a81aac8d589b568d8340d03e7c1b6b9969aebc4
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-09-16 20:55:29 +0200

    adding context

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <h2>Context App</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        )
+    }
+}
