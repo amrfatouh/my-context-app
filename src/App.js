fb09adb938852e229f3ac5288ac8bcefb6bab2ec
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-09-16 21:33:48 +0200

    theme context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
+import ThemeContextProvider from './contexts/ThemeContext'
+import ToggleTheme from './components/ToggleTheme';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+        <ToggleTheme />
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class Navbar extends Component {
+    static contextType = ThemeContext
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <nav>
+            <nav style={{ color: theme.syntax, backgroundColor: theme.ui }}>
                 <h2>Context App</h2>
                 <ul>
                     <li>Home</li>
